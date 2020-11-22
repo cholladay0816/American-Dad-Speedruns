4fa6c3ef6d31f0f8913153f524475a765bcaefa2
Chris H <cholladay0816@gmail.com>
2020-11-21 22:39:03 -0600

    Transition Tweaks

--- resources/views/category/list.blade.php
+++ resources/views/category/list.blade.php
@@ -5,7 +5,8 @@
     <div class="max-w-7xl mx-auto grid grid-cols-3 gap-5 py-10 text-black">
 
         @foreach($categories as $category)
-            <div class="motion-reduce:transition-none motion-reduce:transform-none transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 max-w-sm rounded overflow-hidden shadow-lg bg-white grid grid-cols-2 py-3">
+            <div class="motion-safe:transition-none motion-reduce:transform-none transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110
+             max-w-sm rounded overflow-hidden shadow-lg bg-white grid grid-cols-2 py-3">
                 <a href="{{url('/categories/'.$category->title)}}" class="font-bold text-xl mb-2 text-center col-span-2">
                     {{$category->title}}
                 </a>
