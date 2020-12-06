6f46fdd11ba00a8b907f9d0ce44c66c94856f5c0
Chris H <cholladay0816@gmail.com>
2020-12-05 21:05:09 -0600

    Banner padding adjustment

--- resources/views/livewire/banner.blade.php
+++ resources/views/livewire/banner.blade.php
@@ -1,7 +1,8 @@
 @if(!is_null($banner))
-    <div class=" {{!is_null($banner)?'':'hidden'}} bg-{{$banner->bcolor}} text-{{$banner->tcolor}} flex flex-row p-4 mt-4 max-w-5xl rounded-lg mx-auto text-center text-2xl font-semibold">
+    <div class=" {{!is_null($banner)?'':'hidden'}} bg-{{$banner->bcolor}} text-{{$banner->tcolor}}
+        flex flex-row px-4 py-2 mt-4 max-w-5xl rounded-2xl mx-auto text-center text-2xl font-semibold">
         <a @if(isset($banner->url)) href="{{$banner->url}}" @endif
-        class="flex text-center my-auto mx-auto break-words">
+            class="flex text-center my-auto mx-auto break-words">
             {{$banner->title}}
         </a>
         @auth
