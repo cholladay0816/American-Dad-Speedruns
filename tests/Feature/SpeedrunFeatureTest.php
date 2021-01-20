0e1b0f86edb52bd65ac3faafe0ea4ad0e636ee4c
Chris H <cholladay0816@gmail.com>
2021-01-20 12:44:07 -0600

    Update SpeedrunFeatureTest.php

--- tests/Feature/SpeedrunFeatureTest.php
+++ tests/Feature/SpeedrunFeatureTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\Category;
 use App\Models\Comment;
 use App\Models\Platform;
+use App\Models\Role;
 use App\Models\Speedrun;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -14,6 +15,7 @@ class SpeedrunFeatureTest extends TestCase
 {
     use RefreshDatabase;
 
+    //Generates a new user with a verified email
     public function getUser()
     {
         return User::factory()->create(['email_verified_at'=>now()]);
@@ -22,74 +24,129 @@ class SpeedrunFeatureTest extends TestCase
     /** @test */
     public function guests_cannot_view_or_create_speedruns()
     {
+        // (As a guest) attempt to visit the create speedrun route
         $response = $this->get('/speedruns/new');
+        // Assert the user was redirected due to being unauthorized
         $response->assertStatus(302);
+
+        // (As a guest) submit a post to the speedrun post route
         $response = $this->post('/speedruns/new',
             ['url'=>'https://youtu.be/a', 'time'=>0.1, 'category'=>Category::factory()->create()->id, 'platform'=>Platform::factory()->create()->id]);
+        // Assert the user was redirected due to being unauthorized
         $response->assertStatus(302);
 
     }
     /** @test */
     public function a_logged_in_verified_user_can_view_create_speedruns()
     {
+        // Create and login as a user
         $this->actingAs($this->getUser());
 
+        // Try to visit the new speedrun route
         $response = $this->get('/speedruns/new');
+        // Assert that the user visits the page successfully
         $response->assertStatus(200);
     }
     /** @test */
     public function a_speedrun_must_have_a_working_url()
     {
+        // Create and login as a user
         $this->actingAs($this->getUser());
 
+        // Try to post an empty link
         $response = $this->post('/speedruns/new',
             ['url'=>'', 'time'=>0.1, 'category'=>Category::factory()->create()->id, 'platform'=>Platform::factory()->create()->id]);
+
+        // Assert that the controller failed to validate
         $response->assertSessionHasErrors('url');
+
+        // Try to post an invalid link
         $response = $this->post('/speedruns/new',
             ['url'=>'a', 'time'=>0.1, 'category'=>Category::factory()->create()->id, 'platform'=>Platform::factory()->create()->id]);
+
+        // Assert the controller failed to validate
         $response->assertSessionHasErrors('url');
     }
 
     /** @test */
     public function a_speedrun_can_be_created()
     {
+        // Create and login as a user
         $user = $this->getUser();
         $this->actingAs($user);
 
+        // Create a new speedrun
         $response = $this->post('/speedruns/new',
             ['url'=>'https://youtu.be/a', 'time'=>0.1, 'category'=>Category::factory()->create()->id, 'platform'=>Platform::factory()->create()->id]);
-        $response->assertRedirect('/runner/' . $user->name);
+        // Assert the post was successful
         $response->assertSessionHas('success');
+        // Assert we go back to the user's run page
+        $response->assertRedirect('/runner/' . $user->name);
     }
 
     /** @test */
     public function a_speedrun_can_be_viewed()
     {
+        // Create and login as a user
         $user = $this->getUser();
         $this->actingAs($user);
+
+        // Create a speedrun
         $response = $this->post('/speedruns/new',
             ['url'=>'https://youtu.be/a', 'time'=>0.1, 'category'=>Category::factory()->create()->id, 'platform'=>Platform::factory()->create()->id]);
         $run = Speedrun::first();
 
+        // Assert the run exists and does not 404
         $response = $this->get('/watch/' . $run->id);
-        $response->assertSeeText($user->name);
+        $response->assertOk();
+    }
+
+    /** @test */
+    public function a_speedrun_can_be_disqualified()
+    {
+        // Create and login as user
+        $user = $this->getUser();
+        $this->actingAs($user);
+
+        // Populate database with admin roles
+        $this->seed();
+
+        // Set user to admin
+        $user->roles()->sync([Role::where('name', 'administrator')->first()->id]);
+
+        // Create a speedrun
+        $this->post('/speedruns/new',
+            ['url'=>'https://youtu.be/a', 'time'=>0.1, 'category'=>Category::factory()->create()->id, 'platform'=>Platform::factory()->create()->id]);
+        $run = Speedrun::first();
+
+        // Disqualify the run
+        $this->post(route('disqualify.store', ['speedrun'=>$run]), ['reason'=>'Test Disqualification']);
+
+        // Assert that the run is now disqualified for visitors
+        $response = $this->get('/watch/' . $run->id);
+        $response->assertSeeText('Test Disqualification');
     }
 
     /** @test */
     public function a_speedrun_can_be_removed()
     {
+        // Create and login as a user
         $user = $this->getUser();
         $this->actingAs($user);
+
+        // Submit a new speedrun
         $response = $this->post('/speedruns/new',
             ['url'=>'https://youtu.be/a', 'time'=>0.1, 'category'=>Category::factory()->create()->id, 'platform'=>Platform::factory()->create()->id]);
         $speedrun = $user->fresh()->speedruns->first();
 
+        // Delete the speedrun using the delete run
         $this->delete('/speedruns/' . $speedrun->id);
 
+        // Assert that when the watch page is visited, a 404 appears, meaning the run was deleted successfully.
         $response = $this->get('/watch/' . $speedrun->id);
-
         $response->assertNotFound();
 
     }
-    // Disqualify, Undo, and Remove (Admin)
+
+    // TODO: Undo DQ, and Remove (Admin)
 }
