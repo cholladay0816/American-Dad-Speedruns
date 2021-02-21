253cf8541fc5535feac7f2e77e8770f3862fd865
Chris H <cholladay0816@gmail.com>
2021-02-21 00:37:31 -0600

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,14 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Cache;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function tearDown(): void
+    {
+        Cache::flush();
+    }
 }
