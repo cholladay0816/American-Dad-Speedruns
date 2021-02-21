d0a640dc08c4902ee2424bd8b09fe757d533c615
Chris H <cholladay0816@gmail.com>
2021-02-20 23:43:56 -0600

    Update Message.php

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
