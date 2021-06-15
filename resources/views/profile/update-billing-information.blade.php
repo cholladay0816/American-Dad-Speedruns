13b3e2db12c012d7ab681c4c6bce7c67830e11e8
Christian Holladay <cholladay0816@gmail.com>
2021-06-14 20:14:44 -0500

    Show list of subscriptions, allow cancel

--- resources/views/profile/update-billing-information.blade.php
+++ resources/views/profile/update-billing-information.blade.php
@@ -21,5 +21,35 @@
                 {{ __('Visit Stripe') }}
             </x-jet-button>
         </div>
+        @if(auth()->user()->subscribed())
+        <div class="border-t border-gray-600 mt-3 text-black">
+            <div class="py-2 ">Subscriptions:</div>
+            @foreach(auth()->user()->subscriptions as $subscription)
+                <form method="POST" action="{{route('council.destroy')}}">
+                    @method('DELETE')
+                    @csrf
+                <div class="
+                @if($subscription->stripe_status == 'active' && is_null($subscription->ends_at))
+                bg-green-400
+                @else
+                bg-gray-500
+                @endif
+                text-white rounded-md flex justify-between">
+
+                    <div class="my-auto px-2">
+                        American Dad Speedrun Council Subscription
+                    </div>
+                    @if(is_null($subscription->ends_at))
+                    <button type="submit" class="bg-transparent duration-100 hover:bg-red-400 px-2 py-3 rounded-r text-green-100 hover:text-white">Cancel</button>
+                    @else
+                        <div class="px-2 py-3">
+                            Expiration: {{ \Carbon\Carbon::parse($subscription->ends_at)->diffForHumans() }}
+                        </div>
+                    @endif
+                </div>
+                </form>
+            @endforeach
+        </div>
+        @endif
     </x-slot>
 </x-jet-action-section>
